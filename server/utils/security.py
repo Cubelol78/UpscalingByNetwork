24fd7b09b9688e732d42b4093fd20ec82a75b6dc
Cubelol78 <nico1ereclase@gmail.com>
2025-07-22 22:37:30 +0200

    fix: Add missing logger import in security.py for improved logging functionality

--- server/utils/security.py
+++ server/utils/security.py
@@ -5,6 +5,8 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+from utils.logger import get_logger
+
 class SecurityManager:
     """Gestionnaire de sécurité pour le chiffrement des données"""
     
